59d34bad6152989aa5ba9be46310bbabc6bf1ecf
yurippeeee <isshi0929@icloud.com>
2022-03-13 03:59:02 +0900

    Rust for Windowsのテストプログラム作成

---
+++ Technical_study/rust_for_windows_get_started/rss_reader/src/main.rs
@@ -0,0 +1,16 @@
+use bindings::{ 
+    Windows::Foundation::Uri,
+    Windows::Web::Syndication::SyndicationClient,
+};
+
+fn main() -> windows::Result<()> {
+    let uri = Uri::CreateUri("https://blogs.windows.com/feed")?;
+    let client = SyndicationClient::new()?;
+    let feed = client.RetrieveFeedAsync(uri)?.get()?;
+
+    for item in feed.Items()? {
+        println!("{}", item.Title()?.Text()?);
+    }
+
+    Ok(())
+}
